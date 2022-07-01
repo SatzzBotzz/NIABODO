9bc2b5efa42a12b6859c72f54c9d497f28e7c5b2
SatzzBotzz <101557898+SatzzBotzz@users.noreply.github.com>
2022-07-01 22:18:34 +0700

    NIABODO

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/index.html')
+    res.sendFile(__path + '/index.html'),
+    res.sendFile(__path + '/page1.html'),
+    res.sendFile(__path + '/page2.html'),
+    res.sendFile(__path + '/page3.html'),
+    res.sendFile(__path + '/page4.html')
 })
 module.exports = router
